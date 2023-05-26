5efb637e97ff6600e2d00d7871d36cf7d485102c
newtotems <71979980+newtotems@users.noreply.github.com>
2023-05-26 17:06:49 +0100

    initial config

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Go Monotone",
-	url: "https://example.com/",
+	url: "https://gomonotone.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Go Monotone!",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Go Monotone",
+		email: "info@gomonotone.com",
+		url: "https://gomonotone.com/"
 	}
 }
